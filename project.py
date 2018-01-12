9fe2e9461f1b405b7c25bf5bc13c8ccd86731ff4
VisedFeline121 <30751326+VisedFeline121@users.noreply.github.com>
2018-01-13 01:04:25 +0200

    Add files via upload

---
+++ project.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import socket, nmap
+
+
+def menu():
+    return """1. Get IPs list.
+    2. Get information about a device.
+    3. Tunnel through device.
+    4. Exit.
+    (1/2/3/4)
+    """
+
+
+def get_list():
+    nm = nmap.PortScanner()
+    return nm.all_hosts()
+    #return [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")]
+
+
+#def Get_Info():
+
+
+#def Tunnel():
+
+
+def handle(ans, client):
+    if ans == 1:
+        print ', '.join(get_list())
+
+
+
+def check(ans):
+    return ans in [1, 2, 3]
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ans = input(menu())
+    while ans != 4:
+        if check(ans):
+            handle(ans, client)
+        else:
+            print "Invalid request"
+        ans = input(menu())
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
