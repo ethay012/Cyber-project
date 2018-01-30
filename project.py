b11041449cf8fc0ca3a1e297240539745151f023
Ethay Tam <ethay012@gmail.com>
2018-01-31 00:18:39 +0200

    Tunneling with putty link and opening firefox

--- project.py
+++ project.py
@@ -5,9 +5,13 @@ import random
 import os
 import paramiko
 import re
+import subprocess
+import pyautogui
+import time
+from selenium import webdriver
 
 
-default = [("ubuntu", ""), ("admin", "password")]
+default = [("ubuntu", ""), ("admin", "password"), ("admin", "PoopyMonkeys")]
 
 
 def menu():
@@ -71,21 +75,54 @@ def handle(ans, client):
     if ans == 3:
         init_tunnel()
 
+# def type_credentials(name, password):
+#     time.sleep(3)
+#     pyautogui.typewrite(name)
+#     time.sleep(3)
+#     pyautogui.typewrite(password)
 
 def check(ans):
     return ans in [1, 2, 3]
 
+def install_firefox_proxy(PROXY_HOST,PROXY_PORT):
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference("network.proxy.type", 1)
+    fp.set_preference("network.proxy.socks", PROXY_HOST)
+    fp.set_preference("network.proxy.socks_port", int(PROXY_PORT))
+    fp.set_preference("network.proxy.socks_remote_dns", True)
+    fp.update_preferences()
+    return webdriver.Firefox(firefox_profile=fp)
 
+def TunnelPuttyLink(ip):
+    driver = install_firefox_proxy("127.0.0.1", 3200) # Should be after finding the right ip pass and name
+                                                      # but doesn't work
+    driver.get('about:config')
+    for name, password in default:
+        try:
+            #os.system("plink -D 3200 -pw %s %s@%s" % (password, name, ip))
+            command = "plink -D 3200 -pw %s %s@%s" % (password, name, ip)  # the shell command
+            print subprocess.check_output(command)
+
+        except Exception as err:
+            print "an error occured: " + str(err)
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ans = input(menu())
-    while ans != 4:
-        if check(ans):
-            handle(ans, client)
-        else:
-            print "Invalid request"
-        ans = input(menu())
-    client.close()
+#    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    ans = input(menu())
+#    while ans != 4:
+#        if check(ans):
+#            handle(ans, client)
+#        else:
+#            print "Invalid request"
+#        ans = input(menu())
+#    client.close()
+
+    name = "admin"
+    password = "PoopyMonkeys"
+    ip = "192.168.1.42"
+    TunnelPuttyLink(ip)
+
+#    subprocess.call("plink -D 3200 -pw PoopyMonkeys admin@192.168.1.42")
+#    type_credentials("admin\n", "PoopyMonkeys\n")
 
 
 if __name__ == '__main__':
